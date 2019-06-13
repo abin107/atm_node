957ec6d961919535a91f64972ccd5301182f0952
abin <abin2abraham@yahoo.co.in>
2019-06-13 23:02:55 +0530

    Added test cases

--- app/routes/atm_routes.js
+++ app/routes/atm_routes.js
@@ -1,24 +1,33 @@
 module.exports = function(app){
 
     app.post('/atm' , (req,res) =>{
-        
         var cash = Number(req.body.cash);
-        console.log('cash---------------------------------',cash)
         if(cash > 500 ){
-            res.send("Limit Exceded");
-        }else if(cash%20!=0 && cash%50!=0){
-            res.send("Cannot dispense this Amount, choose another amount");
+            res.status(400).send("Limit Exceded");
+        }else if(cash%10!=0 || cash <=0){
+            res.status(400).send("Cannot dispense this Amount, choose another amount" + cash);
         }else{
+
             var five_hundred = parseInt(cash/500);
-            var fifty = parseInt((cash%500) / 50);
-            var twenty = parseInt((cash%50) / 20);
-            
+            cash -= 500*five_hundred;
+            var fifty = parseInt(cash/50);
+            cash -= 50*fifty;
+            var twenty = parseInt(cash/20);
+            if(cash%20 === 10){
+                if(fifty>1){
+                    fifty -= 1;
+                    twenty += 3;
+                }else{
+                    res.send("Cannot dispense this Amount, choose another amount");
+                    return;
+                }
+            }
             const dispensed_cash = {
                 '500$' : five_hundred,
                 "50$" : fifty,
                 '20$' : twenty
             }
-            
+            res.set('Content-type','application/json');
             res.send(dispensed_cash);
         }
     })
--- test/test.js
+++ test/test.js
@@ -5,22 +5,37 @@ let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
 
+chai.should();
+
 chai.use(chaiHttp);
 
+var passCases = [200,130,490,500,140,50];
+var failCases = [550,1,0,33];
+
 describe('CashDispense Api', ()=>{
-    it('should should dispens valid amounts based on the available denominations',(done) => {
 
-        let params = {
-            cash : '200'
-        }
-
-        chai.request(server)
-            .post('/atm')
-            .send(params)
-            .end((err,res)=>{
-                res.should.have.status(200)
-                res.body.should.have.property("fifty").eql(4);
-                done();
-            });
+    passCases.forEach((a) => {
+        it('should should dispens valid amounts based on the available denominations',(done) => {
+            chai.request(server)
+                .post('/atm')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({'cash':a})
+                .end((err,res)=>{
+                    res.should.have.status(200)
+                    done();
+                });
+        });
+    });
+    failCases.forEach((a) => {
+        it('should throw error when entered invalid amount',(done) => {
+            chai.request(server)
+                .post('/atm')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({'cash':a})
+                .end((err,res)=>{
+                    res.should.have.status(400)
+                    done();
+                });
+        });
     });
 });
